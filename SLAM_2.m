f75c6c8675acf893c8967e7c07282d3772d1cb81
GiacomoCorradini <corra981@gmail.com>
2023-07-04 16:47:31 +0200

    Update uncertainty SLAM_2

--- SLAM_2.m
+++ SLAM_2.m
@@ -250,204 +250,124 @@ end
 
 
 
-%%
-% %% Sensors uncertainty
-% 
-% % -------------------------------------------------------------------------
-% % CAMERA -> give the angle between the forward axis and the line passing
-% % through the center of the robot and the object
-% mu_camera = 0;        % mean value -> 0 means calibrated
-% sigma_camera = 10e-3;  % variance
-% 
-% for i = 1:length(camera_cell)
-%     cameraSensor{i} = camera_cell{i} + randn(2,length(camera_cell{i}))*sigma_camera + mu_camera*ones(2,length(camera_cell{i}));
-% end
-% 
-% % -------------------------------------------------------------------------
-% % % GPS -> give the position and orientation of the robot2
-% % mu_GPS = 0;           % mean value -> 0 means calibrated
-% % sigma_GPS = 1e-2;     % variance
-% % mu_th_GPS = 0;           % mean value -> 0 means calibrated
-% % sigma_th_GPS = 1e-3;     % variance
-% % 
-% % s_1GPS(1:2,:) = s_r1(1:2,:) + randn(2,length(s_r1)).*sigma_GPS + mu_GPS.*ones(2,length(s_r1));
-% % s_2GPS(1:2,:) = s_r2(1:2,:) + randn(2,length(s_r2)).*sigma_GPS + mu_GPS.*ones(2,length(s_r2));
-% % s_1GPS(3,:)   = s_r1(3,:)   + randn(1,length(s_r1)).*sigma_th_GPS + mu_th_GPS.*ones(1,length(s_r1));
-% % s_2GPS(3,:)   = s_r2(3,:)   + randn(1,length(s_r2)).*sigma_th_GPS + mu_th_GPS.*ones(1,length(s_r2));
-% 
-% % -------------------------------------------------------------------------
-% % INPUT -> input velocity of the robot
-% mu_u = zeros(3,1);        % mean value -> 0 means calibrated
-% sigma_u = 1e-2;  % covariance
-% 
-% u_1bar = u_1 + randn(3,length(u_1)).*sigma_u + mu_u.*ones(3,length(s_r1));
-% u_2bar = u_2 + randn(3,length(u_2)).*sigma_u + mu_u.*ones(3,length(s_r1));
-% 
-% % figure('Name','Camera noise'), clf, hold on;
-% % plot(t, cameraSensor(1,:) - s_camera(1,:));
-% % plot(t, cameraSensor(2,:) - s_camera(2,:));
-% % title('Camera noise');
-% % legend('noise camera 1','noise camera 2')
-% % xlabel('t [s]'); ylabel('noise [m]');
+%
+%% Sensors uncertainty
 
-%% 
+% -------------------------------------------------------------------------
+% CAMERA -> give the angle between the forward axis and the line passing
+% through the center of the robot and the object
+mu_camera = 0;        % mean value -> 0 means calibrated
+sigma_camera = 10e-4;  % variance
+
+for i = 1:length(camera_cell)
+    cameraSensor{i} = camera_cell{i} + randn(2,length(camera_cell{i}))*sigma_camera + mu_camera*ones(2,length(camera_cell{i}));
+end
+
+% -------------------------------------------------------------------------
+% INPUT -> input velocity of the robot
+mu_u = zeros(3,1);        % mean value -> 0 means calibrated
+sigma_u = 1e-2;  % covariance
+
+u_1bar = u_1 + randn(3,length(u_1)).*sigma_u + mu_u.*ones(3,length(s_r1));
+u_2bar = u_2 + randn(3,length(u_2)).*sigma_u + mu_u.*ones(3,length(s_r1));
+
+figure('Name','Camera noise'), clf, hold on;
+plot(t, cameraSensor{1}(1,:) - camera_cell{1}(1,:));
+plot(t, cameraSensor{1}(2,:) - camera_cell{1}(2,:));
+title('Camera noise');
+legend('noise camera 1','noise camera 2')
+xlabel('t [s]'); ylabel('noise [m]');
+
+%% UNCERTAINTY
+
+s_r1_bar = zeros(length(s0_1),length(t));
+s_r2_bar = zeros(length(s0_2),length(t));
+
+% Store the initial value
+s_r1_bar(:,1) = s0_1;
+s_r2_bar(:,1) = s0_2;
+
+for cT=1:length(t)-1
+    
+    % Robot dynamic update
+    s_r1_bar(:,cT+1) = RobotDynamic(s_r1_bar(:,cT),u_1bar(:,cT),Dt);
+    s_r2_bar(:,cT+1) = RobotDynamic(s_r2_bar(:,cT),u_2bar(:,cT),Dt);
+
+end
+
+s_r2_mob_bar = zeros(length(s0_2),length(t));
+
+% 
+for i=1:3
+    s_r2_mob_bar(i,:) = s_r2_bar(i,:) - ones(1, length(s_r2_bar(i,:))).*s_r2_bar(i,1);
+end
+
+R2_bar = [cos(-s_r2_bar(3,1)) -sin(-s_r2_bar(3,1));
+      sin(-s_r2_bar(3,1))  cos(-s_r2_bar(3,1))];
+  
+s_r2_mob_bar(1:2,:) = R2_bar*s_r2_mob_bar(1:2,:);
+
+%% Calculate exact position of the robot
+
+obj_ground_cell_mob = cell(1,n_obj);
+obj_robot1_cell_mob = cell(1,n_obj);
+obj_robot2_cell_mob = cell(1,n_obj);
+
+for i = 1:length(obj)
+    obj_ground_cell_mob{i} = nan(length(s0_obj),length(t));
+    obj_robot1_cell_mob{i} = nan(length(s0_obj),length(t));
+    obj_robot2_cell_mob{i} = nan(length(s0_obj),length(t));
+end
+
+for i = 1:length(obj)
+    for cT=2:length(t)
+            
+        if ~isnan(cameraSensor{i}(1,cT)) && ~isnan(cameraSensor{i}(1,cT-1))
+            % calculate the position of the object for each time step
+            phi2 = - s_r1_bar(3,cT) + s_r1_bar(3,cT-1);
+            [obj_robot1_cell_mob{i}(1,cT),obj_robot1_cell_mob{i}(2,cT),~,~] = ...
+             object_detection(s_r1_bar(1,cT),s_r1_bar(2,cT),s_r1_bar(3,cT),s_r1_bar(1,cT-1),s_r1_bar(2,cT-1),...
+             phi2,cameraSensor{i}(1,cT),cameraSensor{i}(1,cT-1));
+
+        elseif isnan(cameraSensor{i}(1,cT)) && ~isnan(obj_robot1_cell_mob{i}(1,cT-1))
+            obj_robot1_cell_mob{i}(1,cT) = obj_robot1_cell_mob{i}(1,cT-1);
+            obj_robot1_cell_mob{i}(2,cT) = obj_robot1_cell_mob{i}(2,cT-1);
+        end
+
+        if ~isnan(cameraSensor{i}(2,cT)) && ~isnan(cameraSensor{i}(2,cT-1))
+            phi2 = - s_r2_mob_bar(3,cT) + s_r2_mob_bar(3,cT-1);
+            [obj_robot2_cell_mob{i}(1,cT),obj_robot2_cell_mob{i}(2,cT), ~, ~] = ...
+             object_detection(s_r2_mob_bar(1,cT),s_r2_mob_bar(2,cT),s_r2_mob_bar(3,cT),s_r2_mob_bar(1,cT-1),s_r2_mob_bar(2,cT-1),...
+             phi2,cameraSensor{i}(2,cT),cameraSensor{i}(2,cT-1));
+
+        elseif isnan(cameraSensor{i}(2,cT)) && ~isnan(obj_robot2_cell_mob{i}(1,cT-1))
+            obj_robot2_cell_mob{i}(1,cT) = obj_robot2_cell_mob{i}(1,cT-1);
+            obj_robot2_cell_mob{i}(2,cT) = obj_robot2_cell_mob{i}(2,cT-1);
+        end
+    end
+end
+
+% Plots real dynamics without uncertainty
+
+figure('Name','Robots positions'), clf, hold on, axis equal;
+xlabel( 'x [m]' );
+ylabel( 'y [m]' );
+title('Robot position in time');
+xlim([-40 40])
+ylim([-40 40])
 
 
-% %% Kalman filter
-% 
-% % CAMERA
-% s_camera_est = zeros(length(camera_0),length(t));
-% Pcam = 10^2*eye(length(camera_0));        % our knowledge about the initial position of the robot 1
-% 
-% % GPS
-% ProbGPS = 0.9;
-% 
-% s_r1_est = zeros(length(s0_1),length(t));
-% s_r1_est(:,1) = s_1GPS(:,1);
-% P1 = 10^2*eye(length(s0_1));              % our knowledge about the initial position of the robot 1
-% P1Store = cell(1, length(t));
-% P1Store{1} = eye(length(s0_1))*sigma_GPS;
-% 
-% s_r2_est = zeros(length(s0_2),length(t));
-% s_r2_est(:,1) = s_2GPS(:,1);
-% P2 = 10^2*eye(length(s0_2));              % our knowledge about the initial position of the robot 2
-% P2Store = cell(1, length(t));
-% P2Store{1} = eye(length(s0_2))*sigma_GPS;
-% 
-% obj_ground_est = zeros(length(s0_obj),length(t));
-% obj_robot1_est = zeros(length(s0_obj),length(t));
-% mu_Err = zeros(length(s0_obj)*2,length(t));
-% sigma_Err = zeros(length(s0_obj)*2,length(t));
-% 
-% mu_Err2 = zeros(length(s0_obj),length(t));
-% sigma_Err2 = zeros(length(s0_obj),length(t));
-% 
-% %%
-% for i=1:length(t)-1
-%     %% Robot 1
-%     
-%     fprintf('Iter %d\n',i)
-%     % Prediction step
-%     % s_k+1 = A*s_k + B*u_k
-%     A = eye(3);
-%     B = eye(3)*Dt;
-%     S1EstPred = A*s_r1_est(:,i) + B*u_1bar(:,i);                     % Dyanmics robot 1
-%     P1pred = A*P1*A' + B*sigma_u^2*B';                               % Prior predict
-%     
-%     % Update step
-%     pGPS1 = rand(1);                                                 % probability to have GPS information
-%     if (pGPS1 <= ProbGPS)
-%         H = eye(3);                                                  % z_k = H*Sk
-%         InnCov = H*P1pred*H' + sigma_GPS^2;                          % Covariance of Innovation
-%         W = P1pred*H'/InnCov;                                   % KF gain
-%         s_r1_est(:,i+1) = S1EstPred + W*(s_1GPS(:,i+1)-H*S1EstPred); % Updated state estimate
-%         P1 = (eye(length(s0_1))-W*H)*P1pred;                         % Updated covariance matrix
-%     else
-%         s_r1_est(:,i+1) = S1EstPred;
-%         P1 = P1pred;
-%     end
-% 
-%     P1Store{i+1} = P1;
-% 
-%     %% Robot 2
-%     
-%     % Prediction step
-%     % s_k+1 = A*s_k + B*u_k
-%     A = eye(3);
-%     B = eye(3)*Dt;
-%     S2EstPred = A*s_r2_est(:,i) + B*u_2bar(:,i);                     % Dyanmics robot 1
-%     P2pred = A*P2*A' + B*sigma_u^2*B';                               % Prior predict
-%     
-%     % Update step
-%     pGPS2 = rand(1);                                                 % probability to have GPS information
-%     if (pGPS2 <= ProbGPS)
-%         H = eye(3);                                                  % z_k = H*Sk
-%         InnCov = H*P2pred*H' + sigma_GPS^2;                          % Covariance of Innovation
-%         W = P2pred*H'/InnCov;                                   % KF gain
-%         s_r2_est(:,i+1) = S2EstPred + W*(s_2GPS(:,i+1)-H*S2EstPred); % Updated state estimate
-%         P2 = (eye(length(s0_2))-W*H)*P2pred;                         % Updated covariance matrix
-%     else
-%         s_r2_est(:,i+1) = S2EstPred;
-%         P2 = P2pred;
-%     end
-% 
-%     P2Store{i+1} = P2;
-%  
-%     %% Object detection robot 1
-%     
-%     % calculate the position of the object for each time step
-% %     index = randi([1 i]); 
-% %     phi1 = s_r1_est(3,i+1) - s_r1_est(3,index);
-% %     [obj_ground_est(1,i),obj_ground_est(2,i),obj_robot1_est(1,i),obj_robot1_est(2,i)] = ...
-% %      object_detection(s_r1_est(1,index),s_r1_est(2,index),s_r1_est(3,index),s_r1_est(1,i+1),s_r1_est(2,i+1),...
-% %      phi1,s_camera(1,i),s_camera(1,i+1));
-% 
-%     % the error must be propagate
-% 
-% %     for j = 1:i
-% %         valuelist = [s_r1_est(1,j),s_r1_est(2,j),s_r1_est(3,j),...
-% %                  cameraSensor(1,j),s_r1_est(1,i+1),s_r1_est(2,i+1),s_r1_est(3,i+1),cameraSensor(1,i+1)];
-% %         errlist = [sqrt(P1Store{j}(1,1)),sqrt(P1Store{j}(2,2)),sqrt(P1Store{j}(3,3)),sigma_camera,...
-% %                    sqrt(P1(1,1)),sqrt(P1(2,2)),sqrt(P1(3,3)),sigma_camera];
-% %         [~,tmp_sigma_Err] = PropError(xA,varlist,valuelist,errlist);
-% % %         [~,tmp_sigma_Err] = PropError(yA,varlist,valuelist,errlist);
-% %         if tmp_sigma_Err < tmp || j == 1
-% %             index = j;
-% %             tmp = tmp_sigma_Err;
-% %         end
-% %     end
-%     
-%     if i >= 20
-%         tmp_sigma_Err = 0;
-%         while abs(tmp_sigma_Err) < 0.1       
-%         j = randi([10,i]);
-%         tmp_sigma_Err = s_r1_est(3,i+1) - s_r1_est(3, j) + cameraSensor(1,i+1) - cameraSensor(1,j);
-%         end
-%     else, j = i;
-%     end
-% 
-%     valuelist = [s_r1_est(1,j),s_r1_est(2,j),s_r1_est(3,j),...
-%                  cameraSensor(1,j),s_r1_est(1,i+1),s_r1_est(2,i+1),s_r1_est(3,i+1),cameraSensor(1,i+1)];
-%     errlist = [sqrt(P1Store{j}(1,1)),sqrt(P1Store{j}(2,2)),sqrt(P1Store{j}(3,3)),sigma_camera,...
-%                sqrt(P1(1,1)),sqrt(P1(2,2)),sqrt(P1(3,3)),sigma_camera];
-%     [mu_Err(1,i),sigma_Err(1,i)] = PropError(xA,varlist,valuelist,errlist);
-%     [mu_Err(2,i),sigma_Err(2,i)] = PropError(yA,varlist,valuelist,errlist);
-% 
-%     if i >= 15
-%         tmp_sigma_Err = 0;
-%         while abs(tmp_sigma_Err) < 0.1       
-%         j = randi([7,i]);
-%         tmp_sigma_Err = s_r2_est(3,i+1) - s_r2_est(3, j) + cameraSensor(2,i+1) - cameraSensor(2,j);
-%         end
-%     else, j = i;
-%     end
-% 
-%     valuelist = [s_r2_est(1,j),s_r2_est(2,j),s_r2_est(3,j),...
-%                  cameraSensor(2,j),s_r2_est(1,i+1),s_r2_est(2,i+1),s_r2_est(3,i+1),cameraSensor(2,i+1)];
-%     errlist = [sqrt(P2Store{j}(1,1)),sqrt(P2Store{j}(2,2)),sqrt(P2Store{j}(3,3)),sigma_camera,...
-%                sqrt(P2(1,1)),sqrt(P2(2,2)),sqrt(P2(3,3)),sigma_camera];
-%     [mu_Err(3,i),sigma_Err(3,i)] = PropError(xA,varlist,valuelist,errlist);
-%     [mu_Err(4,i),sigma_Err(4,i)] = PropError(yA,varlist,valuelist,errlist);
-% 
-% %     %% Object detection robot 2
-% %     
-% %     % calculate the position of the object for each time step
-% %     phi2 = s_r2(3,cT) - s_r1(3,cT);
-% %     [obj_ground(1,cT),obj_ground(2,cT),obj_robot1(1,cT),obj_robot1(2,cT)] = ...
-% %      object_detection(s_r1(1,cT),s_r1(2,cT),s_r1(3,cT),s_r2(1,cT),s_r2(2,cT),...
-% %      phi2,s_camera(1,cT),s_camera(2,cT));
-% % 
-% %     % the error must be propagate
-% 
-% 
-% %     valuelist = [s_r1_est(1,i+1),s_r1_est(2,i+1),s_r1_est(3,i+1),...
-% %                  cameraSensor(1,i+1),s_r2_est(1,i+1),s_r2_est(2,i+1),s_r2_est(3,i+1),cameraSensor(2,i+1)];
-% %     errlist = [sqrt(P1Store{i+1}(1,1)),sqrt(P1Store{i+1}(2,2)),sqrt(P1Store{i+1}(3,3)),sigma_camera,...
-% %                sqrt(P2Store{i+1}(1,1)),sqrt(P2Store{i+1}(1,1)),sqrt(P2Store{i+1}(1,1)),sigma_camera];
-% %     [mu_Err2(1,i),sigma_Err2(1,i)] = PropError(xA,varlist,valuelist,errlist);
-% %     [mu_Err2(2,i),sigma_Err2(2,i)] = PropError(yA,varlist,valuelist,errlist);
-% end
-% 
-%% PLots
-
-% plots;
+for i = 1:3:length(t)-1  
+        phi2 = s_r2_bar(3,i) - s_r1_bar(3,i);
+        for j = 1:length(obj)
+        [p1(j), p2(j), p11, p22] = plot_location2(s_r1_bar(1,i),s_r1_bar(2,i),s_r1_bar(3,i),s_r2_bar(1,i),s_r2_bar(2,i),phi2,...
+                      obj{j}(1),obj{j}(2),cameraSensor{j}(1,i),cameraSensor{j}(2,i),color(j),color(j+10), cameraSensor{n_obj+1}(:,i));
+        
+        drawnow
+        if ~isempty(p11), delete(p11), end
+        if ~isempty(p22), delete(p22), end
+        end
+        if ~isempty(p1), delete(p1), end
+        if ~isempty(p2), delete(p2), end
+        disp(['Iter', num2str(i), ' - obj1 = ' num2str(sum(~isnan(cellfun(@(v)v(1,i),cameraSensor)))), ', obj2 = ', num2str(sum(~isnan(cellfun(@(v)v(2,i),cameraSensor))))])
+end
